474f160b858a97a97a32904ad1b544d80d782a6d
i.sylvin@outlook.com <surfboard@localhost.localdomain>
2020-11-23 11:20:27 +0100

    Frustration push. Trying new approach

--- container/exports.sh
+++ container/exports.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-mkdir -p ~/singu
-echo export SINGULARITY_LOCALCACHEDIR="~/singu"
-echo export SINGULARITY_CACHEDIR="~/singu"
-echo export SINGULARITY_TMPDIR="~/singu"
+mkdir -p ~/scratch/
+echo export NXF_SINGULARITY_LOCALCACHEDIR="~/scratch/"
+echo export NXF_SINGULARITY_CACHEDIR="~/scratch/"
+echo export NXF_SINGULARITY_TMPDIR="~/scratch/"
+echo export SINGULARITY_LOCALCACHEDIR="~/scratch/"
+echo export SINGULARITY_CACHEDIR="~/scratch/"
+echo export SINGULARITY_TMPDIR="~/scratch/"
+echo export TMPDIR="~/scratch/"
+echo export TEMPDIR="~/scratch/"
+echo export SINGULARITY_DISABLE_CACHE=false
 
