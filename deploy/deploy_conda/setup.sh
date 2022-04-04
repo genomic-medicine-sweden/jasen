6b4a432fc6a71a367c7076d0504fcc9a9df60915
sylvinite <i.sylvin@outlook.com>
2022-04-04 14:20:53 +0200

    Added conda deployment

---
+++ deploy/deploy_conda/setup.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+scriptdir="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
+NAME=$1
+if [ $NAME ]; then
+    conda env create --name $NAME -f $scriptdir/requirements.txt
+else
+    conda env create -f $scriptdir/requirements.txt
+fi
