2d1cfb98254e9f3e0e7498383448ffba20ba6f86
mhkc <markus.johansson@me.com>
2022-05-20 08:38:33 +0200

    Removed unused BaseModel classes

--- bin/pipeline_result_processor/prp/models/base.py
+++ bin/pipeline_result_processor/prp/models/base.py
@@ -4,16 +4,6 @@ from datetime import datetime
 from pydantic import BaseConfig, BaseModel, Field
 
 
-class DateTimeModelMixin(BaseModel):
-    """Add explicit time stamps to database model."""
-
-    created_at: datetime | None = Field(None, alias="createdAt")
-
-
-class DBModelMixin(DateTimeModelMixin):
-    id: str | None = Field(None)
-
-
 class RWModel(BaseModel):
     """Base model for read/ write operations"""
 
@@ -21,9 +11,3 @@ class RWModel(BaseModel):
         allow_population_by_alias = True
         allow_population_by_field_name = True
         use_enum_values = True
-
-
-class ModifiedAtRWModel(RWModel):
-    """Base RW model that keep reocrds of when a document was last modified."""
-
-    modified_at: datetime = Field(datetime.now(), alias="modifiedAt")
