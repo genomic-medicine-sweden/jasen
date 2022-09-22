784c9e2971303f2337550c68d2e73fc9a1545372
sylvinite <i.sylvin@outlook.com>
2022-09-22 14:57:41 +0200

    Dependency for output now works

--- deploy/deploy_conda/setup.sh
+++ deploy/deploy_conda/setup.sh
@@ -5,6 +5,12 @@ scriptdir="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 NAME=$1
 if [ $NAME ]; then
     conda env create --name $NAME -f $scriptdir/requirements.txt
+    $scriptdir/../..//bin/pipeline_result_processor
+    pip install .
+    cd $scriptdir
 else
     conda env create --name jasen -f $scriptdir/requirements.txt
+    $scriptdir/../..//bin/pipeline_result_processor
+    pip install .
+    cd $scriptdir
 fi
