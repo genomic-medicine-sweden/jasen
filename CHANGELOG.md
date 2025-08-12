794faa2db23820010966c28583f29f5a2dd97544
Ryan James Kennedy <ryanjameskennedy@icloud.com>
2025-08-12 13:49:10 +0200

    Neaten up mlst db update scripts

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -17,6 +17,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ### Changed
 
 - Removed finder, ShigaPass & tbdb submodules
+- Neatened up mlst db updating scripts
 
 ## [1.1.2]
 
--- assets/mlst_db/update_mlst_db.sh
+++ assets/mlst_db/update_mlst_db.sh
@@ -2,9 +2,18 @@
 
 scriptdir="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 assdir="${scriptdir}/../"
-containerdir="${scriptdir}/../../container/"
+containers_dir="${scriptdir}/../../containers/"
 mntroot=/$(readlink -f .| cut -d"/" -f2)
 
 cd $scriptdir
-bash ./mlst-download_pub_mlst.sh
-singularity exec --bind $mntroot ${containerdir}/blast.sif bash ${assdir}/mlst_db/mlst-make_blast_db.sh
+
+# Download MLST data
+if bash ./mlst-download_pub_mlst.sh; then
+    echo "MLST download completed successfully"
+else
+    echo "MLST download failed" >&2
+    exit 1
+fi
+
+# Create BLAST database
+apptainer exec --bind $mntroot ${containers_dir}/blast.sif bash ${assdir}/mlst_db/mlst-make_blast_db.sh
